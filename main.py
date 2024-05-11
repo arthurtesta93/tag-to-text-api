45f02b6c4a0502b60870ab02998b66e5cb9ee54b
arthurtesta93 <arthurtesta@gmail.com>
2024-05-11 20:09:59 -0300

    refactor tag output format

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import logging
 import os
 
 from flask import Flask, request, jsonify
@@ -10,6 +11,23 @@ client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
 app = Flask(__name__)
 
+# Initialize logger
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+# Create console handler and set level to DEBUG
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+
+# Create formatter
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+# Add formatter to ch
+ch.setFormatter(formatter)
+
+# Add ch to logger
+logger.addHandler(ch)
+
 
 @app.route("/generate_tags", methods=["POST"])
 def tags_controller():
@@ -19,7 +37,7 @@ def tags_controller():
         messages=[
             {
                 "role": "system",
-                "content": "You will be provided with a block of text, and your task is to extract a list of tags from it, a minimum of 2 a maximum of 6."
+                "content": "You will be provided with a block of text, and your task is to extract a list of tags from it, a minimum of 2 a maximum of 6. The tags should be preferrably single words. Also, the output should be a comma separated list of tags."
             },
             {
                 "role": "user",
@@ -32,12 +50,16 @@ def tags_controller():
     )
     completion = response.choices[0]
     output_message = completion.message
-    tags = output_message.content.split('\n')[1:]  # Extracting tags from the content
-    formatted_output = "\n".join(tags)
+    logger.debug("Output message: %s", output_message)
+    lines = output_message.content.split('\n')  # Split content by newline character
 
-    return jsonify({'output': formatted_output})
+    tags = []
+    for line in lines:
+        # Extract tags from each line and append to the tags list
+        tags.extend([tag.strip().rstrip(',') for tag in line.split()])
+
+    return jsonify({'output': tags})
 
 
 if __name__ == '__main__':
     app.run(debug=True)
-
